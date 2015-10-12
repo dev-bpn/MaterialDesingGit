4ba8cf2b0df39d299dce65c76778344793149159
dev-bpn <jazzbpn@gmail.com>
2015-10-12 13:56:23 +0545

    Reveal Effect animation

--- app/src/main/java/materialdesinggit/com/materialdesinggit/MainActivity.java
+++ app/src/main/java/materialdesinggit/com/materialdesinggit/MainActivity.java
@@ -1,11 +1,10 @@
 package materialdesinggit.com.materialdesinggit;
 
+import android.animation.Animator;
 import android.app.Activity;
-import android.app.ActivityOptions;
-import android.content.Intent;
 import android.os.Bundle;
-import android.transition.Explode;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.view.Window;
 
 public class MainActivity extends Activity {
@@ -20,8 +19,22 @@ public class MainActivity extends Activity {
 
     // Simple Transition Animation Between the Activities
     public void onClick(View view){
-        getWindow().setExitTransition(new Explode());
-        Intent intent = new Intent(this , Activity2.class);
-        startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
+//        getWindow().setExitTransition(new Explode());
+//        Intent intent = new Intent(this , Activity2.class);
+//        startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
+        startViewAnimation();
+    }
+
+    private void startViewAnimation(){
+        View myView = (View) findViewById(R.id.textView);
+
+        int cx = myView.getWidth()/2;
+        int cy = myView.getHeight()/2;
+
+        int finalRadius = Math.max(myView.getWidth() , myView.getHeight());
+
+        Animator anim = ViewAnimationUtils.createCircularReveal(myView, cx, cy, 0, finalRadius);
+        myView.setVisibility(View.VISIBLE);
+        anim.start();
     }
 }
--- readme.md
+++ readme.md
@@ -11,4 +11,4 @@ Using the MATERIAL THEME (On Branch MaterialTuto)
 1. Color Palette
 2. CardView Using Rect Drawable with elevation
 3. Vector Drawable
-
+4. Reveal Effect Animation
